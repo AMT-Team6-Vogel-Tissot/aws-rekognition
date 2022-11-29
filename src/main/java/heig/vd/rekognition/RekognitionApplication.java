59518803dda87b4359fcc3223fcd901eecddb045
maelle <maelle.vogel@heig-vd.ch>
2022-11-29 16:14:50 +0100

    correction

---
+++ src/main/java/heig/vd/rekognition/RekognitionApplication.java
@@ -0,0 +1,13 @@
+package heig.vd.rekognition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RekognitionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RekognitionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/heig/vd/rekognition/RekognitionApplicationTests.java
@@ -0,0 +1,13 @@
+package heig.vd.rekognition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RekognitionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
